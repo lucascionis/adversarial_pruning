bde51c262665143553c461e34f741e8a3f02937d
Luca Scionis <lucascionis@tiscali.it>
2023-08-21 18:36:53 +0200

    Update download_harp_pretrained.sh

--- download_harp_pretrained.sh
+++ download_harp_pretrained.sh
@@ -4,4 +4,4 @@ echo 'Downloading HARP pretrained models ...'
 wget https://www.dropbox.com/s/itz7fxphtoqbbwf/trained_models.zip
 echo 'Unzip HARP pretrained models ...'
 mv trained_models.zip harp_pretrained.zip
-unzip trained_models.zip -d ./
+unzip harp_pretrained.zip -d ./
